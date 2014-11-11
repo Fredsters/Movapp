d6d61fe5d01fea38b64f954ae92a3d8475de2921
Fredsters <fred182@gmx.de>
2014-11-11 16:24:39 +0100

    Merged Ollis again

--- app/src/main/java/semanticweb/hws14/movapp/helper/InputCleaner.java
+++ app/src/main/java/semanticweb/hws14/movapp/helper/InputCleaner.java
@@ -11,23 +11,10 @@ import java.util.regex.Pattern;
  */
 public class InputCleaner {
 
-    public static String cleanActorName(String actorName) {
+    public static String cleanName(String actorName) {
         actorName = actorName.trim();
         final StringBuilder result = new StringBuilder(actorName.length());
         String[] words = actorName.split("\\s");
-        for(int i=0; i<words.length; i++) {
-            if(i>0) {
-                result.append(" ");
-            }
-            result.append(Character.toUpperCase(words[i].charAt(0))).append(words[i].substring(1).toLowerCase());
-        }
-        return result.toString();
-    }
-
-    public static String cleanDirectorName(String directorName) {
-        directorName = directorName.trim();
-        final StringBuilder result = new StringBuilder(directorName.length());
-        String[] words = directorName.split("\\s");
         for(int i=0; i<words.length; i++) {
             if(i>0) {
                 result.append(" ");
